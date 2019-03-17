fcb550b083ac7abea88ddb285b82ed5d352c2181
Bryan Hawkins <bhawkins3729@gmail.com>
2019-03-17 13:30:53 -0500

    finished req

--- higher-order.js
+++ higher-order.js
@@ -48,7 +48,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element, index, wholeArray){});
 
 
 
